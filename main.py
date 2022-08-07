d295d75cb1c40121f6277444728193179798b163
Chandu S <93590934+FlareWings@users.noreply.github.com>
2022-08-08 03:29:50 +0530

    Experiment - 01e

--- main.py
+++ main.py
@@ -54,9 +54,9 @@ if __name__ == "__main__":
     # n_estimators = int(args.n_estimators)
     
     # criterion = object(sys.argv[1]) if len(sys.argv) > 1 else 0.5
-    learning_rate = float(sys.argv[1]) if len(sys.argv) > 2 else 0.5
-    max_leaf_nodes = int(sys.argv[2]) if len(sys.argv) > 3 else 0.5
-    n_estimators = int(sys.argv[3]) if len(sys.argv) > 4 else 0.5
+    learning_rate = float(sys.argv[1]) if len(sys.argv) > 1 else 0.5
+    max_leaf_nodes = int(sys.argv[2]) if len(sys.argv) > 2 else 0.5
+    n_estimators = int(sys.argv[3]) if len(sys.argv) > 3 else 0.5
     
 
     with mlflow.start_run():
