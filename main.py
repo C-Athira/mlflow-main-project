15d00a619fdb17d6e08cba4e5bde1c4bddef328b
Chandu S <93590934+FlareWings@users.noreply.github.com>
2022-08-08 03:11:28 +0530

    Experiment - 01b

--- main.py
+++ main.py
@@ -53,25 +53,24 @@ if __name__ == "__main__":
     # max_leaf_nodes = int(args.max_leaf_nodes)
     # n_estimators = int(args.n_estimators)
     
-    criterion = object(sys.argv[1]) if len(sys.argv) > 1 else 0.5
-    learning_rate = float(sys.argv[2]) if len(sys.argv) > 2 else 0.5
-    max_leaf_nodes = int(sys.argv[3]) if len(sys.argv) > 3 else 0.5
-    n_estimators = int(sys.argv[4]) if len(sys.argv) > 4 else 0.5
+    # criterion = object(sys.argv[1]) if len(sys.argv) > 1 else 0.5
+    learning_rate = float(sys.argv[1]) if len(sys.argv) > 2 else 0.5
+    max_leaf_nodes = int(sys.argv[2]) if len(sys.argv) > 3 else 0.5
+    n_estimators = int(sys.argv[3]) if len(sys.argv) > 4 else 0.5
 
     with mlflow.start_run():
-        GBRModel = GradientBoostingRegressor(criterion=criterion, learning_rate=learning_rate, max_leaf_nodes=max_leaf_nodes, n_estimators=n_estimators)
+        GBRModel = GradientBoostingRegressor(learning_rate=learning_rate, max_leaf_nodes=max_leaf_nodes, n_estimators=n_estimators)
         GBRModel.fit(X_train_scaled, y_train)
 
         predicted_qualities = GBRModel.predict(X_test_scaled)
 
         (rmse, mae, r2) = eval_metrics(y_test, predicted_qualities)
 
-        print("Gradient Boosting Regressor model (criterion=%f, learning_rate=%f, max_leaf_nodes=%f, n_estimators=%f):" % (criterion, learning_rate, max_leaf_nodes, n_estimators))
+        print("Gradient Boosting Regressor model (learning_rate=%f, max_leaf_nodes=%f, n_estimators=%f):" % (learning_rate, max_leaf_nodes, n_estimators))
         print("  RMSE: %s" % rmse)
         print("  MAE: %s" % mae)
         print("  R2: %s" % r2)
 
-        mlflow.log_param("criterion", criterion)
         mlflow.log_param("learning_rate", learning_rate)
         mlflow.log_param("max_leaf_nodes", max_leaf_nodes)
         mlflow.log_param("n_estimators", n_estimators)
